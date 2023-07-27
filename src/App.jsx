10faa87ac167424aeb1062fea2f57358a1b5b859
Lusgr <lusinegrigoryan.ait@gmail.com>
2023-07-27 22:51:02 +0400

    Create clock

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Clock from './Clock.jsx';
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <h1>MY APP</h1>
+      <Clock />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Clock.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+const Clock = ()=>{
+
+    const [time, setTime] = useState(new Date())
+
+    useEffect(()=>{
+        const interval = setInterval(()=>{
+            setTime(new Date())
+        },1000)
+
+        return()=>{clearInterval(interval)}
+    },[])
+
+    const formateTime = (date)=>{
+        const hours = date.getHours()
+        const minutes = date.getMinutes()
+        const seconds = date.getSeconds()
+
+        return `${hours} : ${minutes} : ${seconds}`
+    }
+
+
+    return (
+        <>
+            <div>
+                <p>Clock</p>
+                <h1>{formateTime(time)}</h1>
+            </div>
+        </>
+    )
+}
+
+export default Clock
